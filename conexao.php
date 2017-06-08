583d84e56b8415612bde9c6caf1f6a9391618453
Ronaldo Sabino <ronaldodoreino@gmail.com>
2017-06-08 08:34:01 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $hostname_config = "localhost";
-    $database_config = "testeBanco";
-    $username_config = "root";
-    $password_config = "";
-    $config = mysqli_connect($hostname_config, $username_config, $password_config,$database_config) or trigger_error(mysql_error(),E_USER_ERROR); 
